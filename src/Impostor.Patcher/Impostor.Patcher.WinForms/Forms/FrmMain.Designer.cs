c1d14137aa6ed7e534c69ae94552dc75c18d3a4f
Vlad <hello@vladanesov.ru>
2020-11-06 05:31:33 +0300

    trying fix text

--- src/Impostor.Patcher/Impostor.Patcher.WinForms/Forms/FrmMain.Designer.cs
+++ src/Impostor.Patcher/Impostor.Patcher.WinForms/Forms/FrmMain.Designer.cs
@@ -88,7 +88,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(54, 216);
+            this.label3.Location = new System.Drawing.Point(50, 216);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(182, 13);
             this.label3.TabIndex = 6;
